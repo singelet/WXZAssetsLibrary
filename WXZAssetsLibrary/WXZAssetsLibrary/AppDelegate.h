48a3c048b9584f8db359756dce1412ca8c2b235d
xunzhou.wu <lackforyour@163.com>
2015-07-21 18:36:05 +0800

    第一次提交项目.....

---
+++ WXZAssetsLibrary/WXZAssetsLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXZAssetsLibrary
+//
+//  Created by singelet on 15/7/21.
+//  Copyright (c) 2015年 singelet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
