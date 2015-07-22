5dc0947f09a2b11292fdbb0d88ac88b13b11cc61
xunzhou.wu <lackforyour@163.com>
2015-07-22 10:39:47 +0800

    modify code.....

---
+++ WXZAssetsLibrary/WXZAssetsLibrary/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  WXZAssetsLibrary
+//
+//  Created by singelet on 15/7/22.
+//  Copyright (c) 2015年 singelet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITabBarController
+
+@end
--- WXZAssetsLibrary/WXZAssetsLibrary/ViewController.m
+++ WXZAssetsLibrary/WXZAssetsLibrary/ViewController.m
@@ -14,14 +14,64 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+#pragma mark -
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
 }
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+
+- (void) dealloc {
+    
+}
+
+#pragma mark - CommonInit Basic
+
+
+#pragma mark - Setup UI createSubviews
+
+
+
+#pragma mark - Data Fetch
+
+#pragma mark - IBActions
+
+#pragma mark - Button Selector Method
+
+
+#pragma mark - Public Method
+
+
+#pragma mark - Private Method
+
+
+#pragma mark - Delegate
+
+
+#pragma mark - Setter and Getter
+
 
 @end
